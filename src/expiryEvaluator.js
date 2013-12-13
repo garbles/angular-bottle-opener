f2932c799f008acf054e65fbd815281e3440770c
Gabriel Scholz <gabe.scholz@zozi.com>
2013-12-12 21:42:33 -0800

    WIP: expiryEvaluator

---
+++ src/expiryEvaluator.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// TODO:
+// - Helper methods
+//   - :now
+//   - :this
+
+angular.module('bottle.opener')
+  .provider('$expiryEvaluator', function () {
+
+    //private functions
+    function _substitute() {
+      // make variable substitutions
+    }
+
+    this.$get = [function() {
+
+      return function(storage, conditionString) {
+      }
+    }];
+
+  });
